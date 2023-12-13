799d1aa84002adb03bdb8d41c38ae1d4720d0fd9
khinoj <kristopher.hinojosa@gmail.com>
2023-12-13 09:27:14 -0500

    file structure, read and schema layout

---
+++ Develop/server/schemas/index.js
@@ -0,0 +1,5 @@
+// require the typeDefs and resolvers, and export them together
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+module.exports = { typeDefs, resolvers };
